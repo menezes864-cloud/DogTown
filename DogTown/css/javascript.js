05511a747b93b52afb7b10f28ef4e88f539d15b3
Axelbad2 <agenoaxel@gmail.com>
2026-08-17 09:16:38 -0300

    mudei as cores das letras

---
+++ DogTown/css/javascript.js
@@ -0,0 +1,25 @@
+alert("olá, estou usando javascript");
+console.log("ola estou usando javascript")
+let nome = prompt("qual seu nome?")
+console.log(nome)
+
+
+
+
+
+const nome = 'Maria';
+let idade = 18;
+console.log(nome);
+alert('Bem vindo á aula de javascript');
+const idade = 45
+console.log(idade)
+console.log()
+console.log(idade)
+
+let produto = "teclado";
+let preco = 150;
+let quantidade = 2;
+
+let total = preco * quantidade;
+
+console.log("preco e produto
